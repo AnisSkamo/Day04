e64cefc01d2e674e7a4f32431801f34216c6af98
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 20:26:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo){
+
+    var quotient;
+    if(numTwo ===0){
+        quoitent = 'Divide by 0! Cannot divide by zero!';
+    } else {
+        var numOne;
+        var numTwo;
+        var quotient;
+
+    }
+    return quotient;
+    
+}
+console.log(main(15, 3));
+console.log(main(25, 5));
+console.log(main(420, 0));
+console.log(main(55, 0));
+module.exports=main;
