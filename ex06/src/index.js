96786ad48d7e14dd4ad7fd2c34fa2991160b350f
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 23:48:09 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,11 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    var myQuotient = (numOne / numTwo).toFixed(1)
+    //Only change code above this line
+    return myQuotient;
+}
 
+console.log(main(5.0, 2.0));
+console.log(main(16.5, 5.5));
+console.log(main(102, 25.5));
+module.exports=main;
