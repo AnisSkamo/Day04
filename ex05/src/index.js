75207d77b7abc0d340cce5a9be0cd41db68e3ac8
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 23:22:52 +0100

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,11 @@
+function main(numOne, NumTwo){
+    //only change code below this line
+    product = numOne * NumTwo
+    //only change code above this line
+    return product;
+}
 
+console.log(main(2.5, 2.0));
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));
+module.exports = main;
