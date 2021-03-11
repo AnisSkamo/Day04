a5912543bc58536e71b348ff2d9e23b100783a4b
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 22:37:10 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,10 @@
+function main(){
+    var myRes = 75;
+    // Only change code below this line
+    myRes = 75++
+    //Only change code above this line
+    return myRes;
+}
 
+console.log(main());
+module.exports = main;
