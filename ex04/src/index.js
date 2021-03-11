de3b2fcdf85268e50f7db8db2e0e02584c80b957
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 23:04:45 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,13 @@
+function main(){
+    //only change code below this line
+    myDecimal= 8.2
+    myDecimalE= 2.54e-3
+    //Only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
 
+console.log(main());
+module.exports = main;
